853dd6904416c0a27016dc89fb614c5a097da662
tunmph37268 <tunmph37268@fpt.edu.vn>
2023-11-13 17:27:35 +0700

    Bắt đầu dự án

---
+++ app/src/main/java/com/example/dan1_nhom1_md18310/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dan1_nhom1_md18310;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DAN1_Nhom1_MD18310"
+include(":app")
