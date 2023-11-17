7a86d090b50b3ed04f6850c339c56756c9f28df1
toandaychuai <toan2kmax@gmail.com>
2023-11-17 12:50:30 +0700

    thêm loại hàng

---
+++ app/src/main/java/com/example/dan1_nhom1_md18310/Model/IteamClick.java
@@ -0,0 +1,9 @@
+package com.example.dan1_nhom1_md18310.Model;
+
+import com.example.dan1_nhom1_md18310.Model.QuanLyLoaiHang;
+
+public interface IteamClick {
+
+
+    void onClickLoaiHang(QuanLyLoaiHang quanLyLoaiHang);
+}
